1d243954c14dfb374bfc3efd4d96b2f83aa92100
Aleksey.Berdnikov <Aleksey.Berdnikov@nexign-systems.com>
2018-09-13 20:28:36 +0300

    Init main

---
+++ wav_processor/include/main.h
@@ -0,0 +1,6 @@
+#include <string>
+
+using namespace std;
+
+
+int main (int argc, char* argv[]);
---
+++ wav_processor/src/main.cpp
@@ -0,0 +1,10 @@
+#include <string>
+
+#include "main.h"
+
+
+int main (int argc, char* argv[]) {
+    printf("WAV PROCESSOR STARTED\n");
+    
+    return 0;
+}
