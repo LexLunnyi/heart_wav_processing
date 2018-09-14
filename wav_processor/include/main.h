871e529e1c67b1b23614d718685aa5e27699fec9
Aleksey.Berdnikov <Aleksey.Berdnikov@nexign-systems.com>
2018-09-14 18:30:39 +0300

    Wav reader skeleton

--- wav_processor/include/main.h
+++ wav_processor/include/main.h
@@ -1,6 +1,9 @@
 #include <string>
 
+#include "WavReader.h"
+
 using namespace std;
 
 
 int main (int argc, char* argv[]);
+void showHelp();
--- wav_processor/src/main.cpp
+++ wav_processor/src/main.cpp
@@ -1,10 +1,33 @@
-#include <string>
-
 #include "main.h"
 
 
 int main (int argc, char* argv[]) {
+    TWavReader wavReader;
+    
     printf("WAV PROCESSOR STARTED\n");
     
+    if (argc < 2) {
+        showHelp();
+        return 0;
+    }
+    
+    string path(argv[1]);
+    printf("    path to wav-file -> %s\n", path.c_str());
+    
+    string error = "";
+    if (!wavReader.init(path, error)) {
+        printf("    error init TWavReader -> %s\n", error.c_str());
+        return 0;
+    }
+    
     return 0;
+}
+
+
+
+void showHelp() {
+    printf("\n");
+    printf("Example:\n");
+    printf("    ./wav_processor path_to_wav/processing.wav\n");
+    printf("\n");
 }
