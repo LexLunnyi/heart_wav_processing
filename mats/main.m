2339812e7deca99bc83f06189a0169f350ed81f7
LexLunnyi <Aleksey.Berdnikov@nexign-systems.com>
2020-02-15 11:56:22 +0300

    Updated mat-scripts for mark up

--- mats/main.m
+++ mats/main.m
@@ -1,5 +1,22 @@
 global indata;
-indata = B001;
-%indata.MANUAL = zeros(length(indata.SIGNAL), 1);
+global index;
+global OUT;
 
-plot_pcg();
+load("matdata/OUT.mat");
+
+index = 0;
+
+for i = 1:length(OUT.INDEX)
+    processed = OUT.PROCESSED(i);
+    if processed == 0
+        index = i;
+        break
+    end
+end
+
+if index > 0
+    fileName = strcat("data/", OUT.PATH(index), "/", OUT.FILE(index));
+    indata = readtable(fileName);
+    indata.MANUAL = zeros(length(indata.SIGNAL), 1);
+    plot_pcg();
+end
