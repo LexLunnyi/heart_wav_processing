a18d3dfcfddaaea2b578596f46d054af103a01ac
LexLunnyi <Aleksey.Berdnikov@nexign-systems.com>
2020-09-29 10:48:08 +0300

    continue refactor

--- src/main/java/org/ll/heart/sound/recognition/PCGWrapper.java
+++ src/main/java/org/ll/heart/sound/recognition/PCGWrapper.java
@@ -1,9 +1,12 @@
 package org.ll.heart.sound.recognition;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import org.ll.heart.sound.recognition.fdomain.FrequencyDomainFFT;
 import org.ll.heart.sound.recognition.fdomain.FrequencyDomainService;
 import org.ll.heart.sound.recognition.wav.WavFile;
@@ -16,17 +19,21 @@ import org.ll.heart.sound.recognition.wav.WavFileException;
 public class PCGWrapper {
     private final Options options;
     private final WavFile wavFile;
-    private final int WINDOW_SIZE;
+    private final int windowSize;
     private final Date WAV_LIMIT_BEGIN;
+    private Integer PCG_LIMIT_BEGIN = null;
     private final Date WAV_LIMIT_END;
+    private Integer PCG_LIMIT_END = null;
+    
     private final double[] data;
+    private final List<SignalPortion> PCG = new ArrayList<>();
     
     FrequencyDomainService fservie;
     
     public PCGWrapper(File in, Options options) throws IOException, WavFileException {
         this.options = options;
-        this.WAV_LIMIT_BEGIN = new Date(options.getWavLengthMin());
-        this.WAV_LIMIT_END = new Date(options.getWavLengthMax());
+        WAV_LIMIT_BEGIN = new Date(options.getWavLengthMin());
+        WAV_LIMIT_END = new Date(options.getWavLengthMax());
         // Open the wav file specified as the first argument
         wavFile = WavFile.openWavFile(in);
         // Display information about the wav file
@@ -39,9 +46,9 @@ public class PCGWrapper {
         //Get window size and step
         if (options.isWindowAuto()) {
             WindowParams autoParams = new WindowParams(options.getWindowFrequencyRate(), (int)wavFile.getSampleRate());
-            WINDOW_SIZE = autoParams.getSize();
+            windowSize = autoParams.getSize();
         } else {
-            WINDOW_SIZE = options.getWindowSize();
+            windowSize = options.getWindowSize();
         }
         //Check record size
         long frameSize = wavFile.getNumFrames();
@@ -56,28 +63,29 @@ public class PCGWrapper {
     
     
     private void readFile() throws IOException, WavFileException {
-        int buffSize = WINDOW_SIZE * wavFile.getNumChannels();
+        int buffSize = windowSize * wavFile.getNumChannels();
         // Create a buffer of N frames
         double[] buffer = new double[buffSize];
         int framesRead;
         long index = 0;
         do {
             // Read frames into buffer
-            framesRead = wavFile.readFrames(buffer, WINDOW_SIZE);
+            framesRead = wavFile.readFrames(buffer, windowSize);
             // Loop through frames and look for minimum and maximum value
-            
             for (int s = 0; s < framesRead * wavFile.getNumChannels(); s++) {
                 index++;
                 int i = (int)index-1;
                 Date ts = new Date((index * 1000) / wavFile.getSampleRate());
-                //FIXME do we need to limit reading here, maybe only processing?
                 if (options.isWavLengthLimited()) {
                     if (ts.compareTo(WAV_LIMIT_BEGIN) < 0) {
                         data[i] = 0.0;
+                    } else if (null == PCG_LIMIT_BEGIN) {
+                        PCG_LIMIT_BEGIN = i;
                     }
                     if (ts.compareTo(WAV_LIMIT_END) > 0) {
-                        framesRead = 0;
                         data[i] = 0.0;
+                    } else {
+                        PCG_LIMIT_END = i;
                     }
                 }
                 data[i] = buffer[s];
@@ -86,14 +94,10 @@ public class PCGWrapper {
     }
     
     
-    public void process(File out) {
+    public void process(String out) throws IOException {
         configure();
-        try {
-            process();
-            save(out);
-        } catch (Exception ex) {
-            System.err.println("ERROR: PCGWrapper.process -> " + ex);
-        }
+        process();
+        save(out);
     }
         
     //Configure services for transorm to frequency domain, filtration and segmentation
@@ -106,17 +110,34 @@ public class PCGWrapper {
     }
     
     //main action
-    private void process() {
-        int size = data.length;
+    private void process() throws IOException {
+        PCG.clear();
         
-        for(int i = 0; i < size - WINDOW_SIZE; i++) {
-            SignalPortion portion = new SignalPortion(Arrays.copyOfRange(data, i, i + WINDOW_SIZE));
+        int start = (null == PCG_LIMIT_BEGIN) ? 0 : PCG_LIMIT_BEGIN;
+        int end = (null == PCG_LIMIT_END) ? data.length - windowSize : PCG_LIMIT_END - windowSize;
+        if (end <= start) {
+            throw new IOException("The start point or end point is incorrect");
+        }
+        
+        for(int i = start; i < end; i++) {
+            //We take midle element of the window
+            int index = i + windowSize / 2;
+            //Calc time
+            Date ts = new Date((index * 1000) / wavFile.getSampleRate());
+            //Create signal portion
+            SignalPortion portion = new SignalPortion(ts, data[index], Arrays.copyOfRange(data, i, i + windowSize));
             fservie.process(portion);
+            PCG.add(portion);
         }
     }
     
     
-    private void save(File out) {
-        
+    private void save(String out) throws IOException {
+        try (FileWriter fileWriter = new FileWriter(out)) {
+            fileWriter.write(PCG.get(0).getCSVColumnsNames(false) + "\n");
+            for (SignalPortion signal : PCG) {
+                fileWriter.write(signal.toCSV());
+            }
+        }
     }
 }
--- src/main/java/org/ll/heart/sound/recognition/SignalPortion.java
+++ src/main/java/org/ll/heart/sound/recognition/SignalPortion.java
@@ -1,17 +1,69 @@
 package org.ll.heart.sound.recognition;
 
+import java.util.Date;
 import org.apache.commons.math3.complex.Complex;
 
 /**
  *
  * @author aberdnikov
  */
+
+
+enum SignalColumn {
+    TIME(0),
+    SIGNAL(1);
+    
+    int index;
+
+    private SignalColumn(int index) {
+        this.index = index;
+    }
+    
+    public int get() {
+        return this.index;
+    }
+}
+
+
+
 public class SignalPortion {
+    Date ts;
+    double source;
     double[] in;
     double[] out;
     Complex[] spectrum;
     
-    public SignalPortion(double[] in) {
+    
+    public SignalPortion(Date ts, double src, double[] in) {
         this.in = in;
+        this.ts = ts;
+        this.source = src;
+        this.in = in;
+    }
+    
+    public String toCSV() {
+        return ts.getTime() + ";" + source + "\n";
+    }
+    
+    public String getCSVColumnsNames(boolean sourceOnly) {
+        StringBuilder sbuf = new StringBuilder();
+        sbuf.append(WavColumn.TIME.name()).append(";");
+        sbuf.append(WavColumn.SIGNAL.name());
+        if (!sourceOnly) {
+//            sbuf.append(";").append(WavColumn.FILTRED.name()).append(";");
+//            sbuf.append(WavColumn.MAGNITUDE.name()).append(";");
+//            sbuf.append(WavColumn.SX.name()).append(";");
+//            sbuf.append(WavColumn.HARMONIC_INDEX.name()).append(";");
+//            sbuf.append(WavColumn.SQUARE_SEMI_WAVE.name()).append(";");
+//            sbuf.append(WavColumn.WINDOWS_ENERGY.name()).append(";");
+//            sbuf.append(WavColumn.TIME_FROM_CHANGE_POINT.name()).append(";");
+//            sbuf.append(WavColumn.TIME_FROM_INFLECTION_POINT.name()).append(";");
+//            sbuf.append(WavColumn.MAGNITUDES_ANGLE.name()).append(";");
+//            sbuf.append(WavColumn.WINDOW_CHANGE_POINTS_CNT.name()).append(";");
+//            sbuf.append(WavColumn.WINDOW_INFLECTION_POINTS_CNT.name()).append(";");
+//            sbuf.append(WavColumn.FIRST_DERIVATIVE.name()).append(";");
+//            sbuf.append(WavColumn.SECOND_DERIVATIVE.name());
+        }
+        return sbuf.toString();
     }
 }
